8274801917b3d0572fad2ce28af1d761836094da
i2i <pankaj.soni@i2ifundings.com>
2018-10-10 01:21:34 -0700

    h

---
+++ app/src/main/java/com/example/i2i/myapplication/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.i2i.myapplication
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        init()
+    }
+
+    private fun init() {
+        Toast.makeText(this@MainActivity,"Testing",Toast.LENGTH_SHORT).show()
+    }
+}
